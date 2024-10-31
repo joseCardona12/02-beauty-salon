bdd6ba22ee5e102ce8f5ff8bdeabc71a855fde67
joseCardona12 <josesiprozmaster@gmail.com>
2024-10-31 08:36:02 -0500

    feat: add structure initial

---
+++ src/ui/molecules/common/FormField.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function FormField():React.ReactNode {
+    return (
+        <div className="">
+            <label htmlFor=""></label>
+            <input type="text" />
+        </div>
+    )
+}
